42411c05e4c2f6605396c1315df000e1b450adc6
miladiashe <dkdud0122@naver.com>
2020-07-13 00:15:09 +0900

    State 사용

--- for-study/src/App.js
+++ for-study/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MyComponent from "./MyComponent";
+import MyClassComponent from "./MyClassComponent";
 
 function App() {
   return (
     <div>
         <MyComponent>칠드런</MyComponent>
+        <MyClassComponent>칠드런1</MyClassComponent>
     </div>
   );
 }
