c222548fa72e9fc462935f082289edc5ed2c92b5
miladiashe <dkdud0122@naver.com>
2020-07-14 00:44:18 +0900

    객체로 이루어진 배열
    과 추가제거 기능 구현

--- for-study/src/IterationSample.js
+++ for-study/src/IterationSample.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-    const names = ['미카엘라', '렌', '미나'];
-    const nameList = names.map(name => <li>{name}</li>);
-    return <ul>{nameList}</ul>
+    const [names, setNames] = useState([
+        { id: 1, text: '미나' },
+        { id: 2, text: '미카엘라' },
+        { id: 3, text: '렌'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5)
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul></>)
 };
 
-export default IterationSample;
+export default IterationSample
