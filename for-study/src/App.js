08af60efc84ba08a7259f6c0e5e1b1999728eded
miladiashe <dkdud0122@naver.com>
2020-07-13 14:14:15 +0900

    useState 실습

--- for-study/src/App.js
+++ for-study/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MyComponent from "./MyComponent";
 import MyClassComponent from "./MyClassComponent";
+import Say from "./Say";
 
 function App() {
   return (
     <div>
         <MyComponent>칠드런</MyComponent>
         <MyClassComponent>칠드런1</MyClassComponent>
+        <Say />
     </div>
   );
 }
